d86ce7efada99c2832681322b5980eb5399c504c
James Bracy <waratuman@gmail.com>
2026-03-19 12:40:19 -0700

    fix: comment out stub function, restore full src/ format check

--- src/StandardApi/Url/Parser/Query.elm
+++ src/StandardApi/Url/Parser/Query.elm
@@ -1,9 +1,10 @@
 module StandardApi.Url.Parser.Query exposing (..)
 
 import StandardApi exposing (Query)
-import Url.Query.Parser
-
-query : Parser Query
-query =
 
 
+-- TODO: Implement query string parser that converts URL query parameters
+-- back into a Query type (inverse of StandardApi.Url.Builder).
+--
+-- query : Parser Query
+-- query =
